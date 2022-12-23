7c84fcfd44c3d6889be62d2d781965a69da71c71
mpraes <renan.de.moraes777@gmail.com>
2022-12-23 16:19:05 -0300

    atualizando script de coleta de dados para ano inteiro

--- baixar.py
+++ baixar.py
@@ -2,24 +2,31 @@ import requests
 import zipfile
 import os
 # URL do arquivo
-print("Armazenando variável do site")
-url = 'https://portaldatransparencia.gov.br/download-de-dados/compras/202201'
+ano = str(input("Informe o ano e mês desta maneira: (yyyy)"))
+
+
+for mes in range(1,12):
+# Definindo url com mês e ano
+    print("Armazenando variável do site")
+    mes_ano = "".join([ano,str(mes).zfill(2)])
+    url = 'https://portaldatransparencia.gov.br/download-de-dados/compras/{}'.format(mes_ano)
+    response = requests.get(url)
+    with open(mes_ano +'.zip', 'wb') as f:
+        f.write(response.content)
+    nome_arquivo = mes_ano + '.zip'
+    # Abre o arquivo ZIP
+    with zipfile.ZipFile(nome_arquivo, 'r') as zip_ref:
+        # Extrai todos os arquivos do arquivo ZIP
+        zip_ref.extractall()
 
 # Faz o download do arquivo
 print("Executando download")
-response = requests.get(url)
+
 
 print("Armazenando em diretório local")
+
 # Escreve o conteúdo do arquivo em um arquivo local
-with open('202201.zip', 'wb') as f:
-    f.write(response.content)
 
-nome_arquivo = '202201.zip'
-
-# Abre o arquivo ZIP
-with zipfile.ZipFile(nome_arquivo, 'r') as zip_ref:
-    # Extrai todos os arquivos do arquivo ZIP
-    zip_ref.extractall()
 
 # Fecha o arquivo ZIP
 zip_ref.close()
---
+++ teste.py
@@ -0,0 +1,5 @@
+mes_ano = 202201
+
+nome_arquivo = str(mes_ano) + '.zip'
+
+print(nome_arquivo)
