af0cbb52dc533e0e4945670b71bf8ebe80271cb6
mpraes <renan.de.moraes777@gmail.com>
2022-12-23 16:29:37 -0300

    atualizando novamenteo

--- baixar.py
+++ baixar.py
@@ -2,34 +2,38 @@ import requests
 import zipfile
 import os
 # URL do arquivo
-ano = str(input("Informe o ano e mês desta maneira: (yyyy)"))
-
+ano = str(input("Informe o ano para baixar os arquivos: (yyyy)"))
 
 for mes in range(1,12):
 # Definindo url com mês e ano
     print("Armazenando variável do site")
-    mes_ano = "".join([ano,str(mes).zfill(2)])
-    url = 'https://portaldatransparencia.gov.br/download-de-dados/compras/{}'.format(mes_ano)
+    ano_mes = "".join([ano,str(mes).zfill(2)])
+    url = 'https://portaldatransparencia.gov.br/download-de-dados/compras/{}'.format(ano_mes)
     response = requests.get(url)
-    with open(mes_ano +'.zip', 'wb') as f:
+    print("Baixando arquivo do ano-mes: " + ano_mes)
+    # Faz o download do arquivo
+    # Escreve o conteúdo do arquivo em um arquivo local
+    print("Armazenando em diretório local")
+    with open(ano_mes +'.zip', 'wb') as f:
         f.write(response.content)
-    nome_arquivo = mes_ano + '.zip'
+    nome_arquivo = ano_mes + '.zip'
     # Abre o arquivo ZIP
     with zipfile.ZipFile(nome_arquivo, 'r') as zip_ref:
         # Extrai todos os arquivos do arquivo ZIP
         zip_ref.extractall()
+    # Fecha o arquivo ZIP
+    zip_ref.close()
+    # Exclui o arquivo ZIP
+    os.remove(nome_arquivo)
 
-# Faz o download do arquivo
-print("Executando download")
+print("Finalizado")
+
+## Precisa verificar por que não está extraindo os dados de novembro de 2022
 
 
-print("Armazenando em diretório local")
-
-# Escreve o conteúdo do arquivo em um arquivo local
 
 
-# Fecha o arquivo ZIP
-zip_ref.close()
 
-# Exclui o arquivo ZIP
-os.remove(nome_arquivo)
+
+
+
